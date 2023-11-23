8af9c87bd4b9d222c3682c9907db287702a84400
metasheet <151730162+metasheet@users.noreply.github.com>
2023-11-23 14:50:12 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
-function downloadVideo() {
-    var videoUrl = document.getElementById('videoUrl').value;
+var API_KEY = 'AIzaSyA1mnNSkE4Wo9KvNLetrFSpTpqJtIISoT8';
 
-    // Construct a download link (replace 'youtubepp.com' with your preferred download service)
-    var downloadLink = 'https://www.youtubepp.com/watch?v=' + getVideoId(videoUrl);
-    window.location.href = downloadLink;
+function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
 }
 
-function getVideoId(url) {
-    // Extract video ID from various YouTube video URL formats
-    var regex = /[?&]v=([^?&]+)/;
-    var match = url.match(regex);
+function initClient() {
+    gapi.client.init({
+        apiKey: API_KEY,
+        discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"]
+    }).then(function () {
+        console.log("YouTube Data API client initialized");
+    });
+}
 
-    return match && match[1] ? match[1] : null;
+function getDownloadLink() {
+    var videoId = document.getElementById('videoId').value;
+
+    gapi.client.youtube.videos.list({
+        part: 'contentDetails',
+        id: videoId
+    }).then(function(response) {
+        var videoDetails = response.result.items[0].contentDetails;
+        var videoDuration = parseVideoDuration(videoDetails.duration);
+        var downloadLink = 'https://www.youtubepp.com/watch?v=' + videoId + '&t=' + videoDuration;
+        
+        document.getElementById('downloadLink').innerHTML = '<a href="' + downloadLink + '" target="_blank">Download Video</a>';
+    }, function(error) {
+        console.error('Error fetching video details', error);
+        document.getElementById('downloadLink').innerHTML = 'Error fetching video details';
+    });
+}
+
+function parseVideoDuration(duration) {
+    var match = duration.match(/PT(\d+H)?(\d+M)?(\d+S)?/);
+
+    var hours = (parseInt(match[1]) || 0);
+    var minutes = (parseInt(match[2]) || 0);
+    var seconds = (parseInt(match[3]) || 0);
+
+    return hours * 3600 + minutes * 60 + seconds;
 }
